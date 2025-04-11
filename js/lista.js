6dca28b130abccce25b27e2997c9d74bc5b4a645
Wilson Carmona <wilsonandres08@hotmail.com>
2025-04-11 04:23:49 +0000

    codigo conexion lista

--- js/lista.js
+++ js/lista.js
@@ -24,5 +24,13 @@ function mostrarLista(pokemones) {
     
     /*app.appendChild(buscador);*/
     app.appendChild(seccion);
-
-}
+    async function  conexionLista() {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025')
+        const data = await res.json()
+        return data.results;
+    }
+    
+    async function General() {
+        const infoPokes = await conexionLista();
+        mostrarLista(infoPokes)
+    }
